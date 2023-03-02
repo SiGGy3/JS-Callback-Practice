d60048333755edf8333859768ca2a3502d28d42d
SiGGy3 <justinjwsig@gmail.com>
2023-03-01 23:56:07 -0800

    never got anything to work have lots of studying to do

--- index.js
+++ index.js
@@ -3,6 +3,49 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
+let direction = null;
+let x = 100;
+let y = 250;
+
+function moveCharacter() {
+    if(direction === 'west') {
+        x = x - 1
+    }
+    if(direction === 'north') {
+        y = y + 1
+    }
+    if(direction === 'east') {
+        x = x + 1
+    }
+    if(direction === 'south') {
+        y = y - 1
+    }
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    console.log(moveCharacter)                                
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
